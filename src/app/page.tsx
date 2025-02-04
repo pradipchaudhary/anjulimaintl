1bc2ab51d942543a04efb25ba90215a5bbfa517d
pradip <chaudharypradip678@gmail.com>
2025-02-05 00:14:29 +0545

    fixed issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import Footer from "@/components/sections/Footer";
-import Header from "@/components/sections/Header";
+import React from 'react'
 
-export default function Home() {
-  return (
-
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-8 font-[family-name:var(--font-geist-sans)]">
-      <Header />
-      <main className="h-[75vh] p-10 flex justify-center items-center">
-        <h1 className="text-7xl">anjulimaintl</h1>
-      </main>
-      <Footer />
-    </div>
-  );
+const Home = () => {
+    return (
+        <div>
+            <h1>anjulimaintl</h1>
+        </div>
+    )
 }
+
+export default Home;
--- src/components/sections/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    const currentYear = new Date().getFullYear();
-    return (
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-            @compright, {currentYear}
-        </footer>
-    )
-}
