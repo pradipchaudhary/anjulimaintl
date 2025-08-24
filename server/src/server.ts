f1bb112a3bc43bf11fbc979cec85ec06fe3091af
Pradip Chaudhary <chaudharypradip678@gmail.com>
2025-08-24 14:55:26 +0545

    Delete server directory

--- server/src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-import { connectDB } from './config/db';
-
-const PORT = process.env.PORT || 5000;
-
-connectDB().then(() => {
-    app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
-});
