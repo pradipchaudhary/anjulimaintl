03f059dee54429285f6037c2c1e9fb5fe7a6a08b
Pradip Chaudhary <chaudharypradip678@gmail.com>
2025-08-24 09:33:09 +0000

    new ui

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1> Welcome to Anjulima Int</h1>
+      <button className="bg-slate-200"> Login </button>
+    </div>
+  );
+}
