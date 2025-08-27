d5c9a2d357ab290ae99e28a9e4feb2695fb7caa1
Pradip Chaudhary <chaudharypradip678@gmail.com>
2025-08-27 13:00:27 +0545

    Update page.tsx

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { signIn } from 'next-auth/react';
+
 
 export default function RegisterPage() {
     const router = useRouter();
