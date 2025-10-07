4cfcb165e44af7c90edfb3bc88f8607c5513a6f3
Pradip Chaudhary <chaudharypradip678@gmail.com>
2025-10-07 16:56:47 +0545

    change eslint.config.mjs file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,10 +19,7 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
+
   },
 
 ];
